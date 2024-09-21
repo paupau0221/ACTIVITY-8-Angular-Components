b1aacf48a39425385d413f4e618f17ed30b4cc7a
paupau0221 <paulinesoler2929@gmail.com>
2024-09-21 09:23:12 +0900

    fibonaccigenerator

--- src/app/components/fibonnaccigenerator/fibonnaccigenerator.component.ts
+++ src/app/components/fibonnaccigenerator/fibonnaccigenerator.component.ts
@@ -7,14 +7,14 @@ import { Component } from '@angular/core';
 })
 export class FibonnaccigeneratorComponent {
   ount: number = 0;
-  fibonacciNumbers: number[] = [];
+  fibonaccigenerator: number[] = [];
   counts: any;
 
   generateFibonacciNumbers() {
-    this.fibonacciNumbers = [];
+    this.fibonaccigenerator = [];
     let n1 = 0, n2 = 1, nextTerm;
     for (let i = 1; i <= this.counts; i++) {
-      this.fibonacciNumbers.push(n1);
+      this.fibonaccigenerator.push(n1);
       nextTerm = n1 + n2;
       n1 = n2;
       n2 = nextTerm;
