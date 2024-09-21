e4e9a27e616e2327f28eaed47428aa3a660c943d
paupau0221 <paulinesoler2929@gmail.com>
2024-09-21 09:22:12 +0900

    compoundinterestcalculator

--- src/app/components/compoundinterestcalculator/compoundinterestcalculator.component.ts
+++ src/app/components/compoundinterestcalculator/compoundinterestcalculator.component.ts
@@ -10,10 +10,10 @@ export class CompoundinterestcalculatorComponent {
   rate: number = 0;
   time: number = 0;
   frequency: number = 1; // Default to annual compounding
-  compoundInterest: number | null = null;
+  compoundinterestcalculator: number | null = null;
 
   calculateCompoundInterest() {
     const r = this.rate / 100; // Convert percentage to decimal
-    this.compoundInterest = this.principal * Math.pow(1 + (r / this.frequency), this.frequency * this.time) - this.principal;
+    this.compoundinterestcalculator = this.principal * Math.pow(1 + (r / this.frequency), this.frequency * this.time) - this.principal;
   }
 }
