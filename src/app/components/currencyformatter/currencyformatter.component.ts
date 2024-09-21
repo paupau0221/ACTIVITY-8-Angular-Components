d68dee56acfbae116c725dc604758d36dc295f49
paupau0221 <paulinesoler2929@gmail.com>
2024-09-21 09:26:47 +0900

    currencyformatter

--- src/app/components/currencyformatter/currencyformatter.component.ts
+++ src/app/components/currencyformatter/currencyformatter.component.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrl: './currencyformatter.component.css'
 })
 export class CurrencyformatterComponent {
+  amount: number = 0;
+  currency: string = 'USD';
+  formattedAmount: string = '';
 
+  formatCurrency() {
+    switch (this.currency) {
+      case 'USD':
+        this.formattedAmount = `$${this.amount.toFixed(2)}`;
+        break;
+      case 'PHP':
+        this.formattedAmount = `₱${this.amount.toFixed(2)}`;
+        break;
+      case 'EUR':
+        const euroAmount = this.convertToEuro(this.amount);
+        this.formattedAmount = `€${euroAmount.toFixed(2)}`;
+        break;
+      default:
+        this.formattedAmount = this.amount.toString();
+    }
+  }
+
+  convertToEuro(amount: number): number {
+    const conversionRate = 0.85; // Example conversion rate from USD to Euro
+    return amount * conversionRate;
+  }
 }
